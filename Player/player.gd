52876655f7376f7f2aca0d8b87c656cbaf1dc4da
Flygohr <info@flygohr.com>
2025-04-30 10:41:08 +0200

    tried changing movement

--- Player/player.gd
+++ Player/player.gd
@@ -1,14 +1,28 @@
-extends CharacterBody2D
+extends Sprite2D
 
-func _input(event):
-	if event.is_action_pressed("ui_right"):
-		position.x += Globals.TILE_SIZE
-		
-	if event.is_action_pressed("ui_left"):
-		position.x -= Globals.TILE_SIZE
-		
-	if event.is_action_pressed("ui_up"):
-		position.y -= Globals.TILE_SIZE
-		
-	if event.is_action_pressed("ui_down"):
-		position.y += Globals.TILE_SIZE
+@onready var _animation_player = $AnimationPlayer
+
+enum PlayerState { IDLE, TURNING, WALKING }
+enum FacingDirection { LEFT, RIGHT, UP, DOWN }
+
+var player_state = PlayerState.IDLE
+var facing_direction = FacingDirection.DOWN
+
+
+# if idle and action pressed is not facing direction -> turn on the spot
+# if idle and action pressed IS facing direction -> move 1
+# if idle and action keep pressed -> keep walking 
+
+
+var hold_counter : float = 0.0
+var hold_time : float = 0.1
+
+func _process(delta):
+	if Input.is_action_pressed("ui_up"):
+		hold_counter += delta
+		_animation_player.play("idleUp")
+		if facing_direction == FacingDirection.UP and hold_counter >= hold_time:
+			position.y -= Globals.TILE_SIZE
+		facing_direction = FacingDirection.UP
+		hold_counter == 0.0
+	
