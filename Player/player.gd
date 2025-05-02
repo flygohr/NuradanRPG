f9b3560a336eec6316db3115a2d0c62ad94f83d5
Flygohr <info@flygohr.com>
2025-05-02 21:25:34 +0200

    responsive movement missing tap to move once

--- Player/player.gd
+++ Player/player.gd
@@ -6,45 +6,71 @@ extends Sprite2D
 
 
 enum playerStates { IDLE, TURNING, WALKING }
+const MOVEMENTS = {
+	'ui_up': Vector2.UP,
+	'ui_left': Vector2.LEFT,
+	'ui_right': Vector2.RIGHT,
+	'ui_down': Vector2.DOWN,
+}
+var direction_history = []
 
 var currentPlayerState = playerStates.IDLE
 var currentFacingDirection : Vector2 = Vector2.DOWN
 
 var inputDirection : Vector2
 var isMoving = false
-var moveSpeed : float = 0.2
+var moveSpeed : float = 0.1
+
+var holdCounter
+var holdTime = 20
 
 func _ready() -> void: 
 	_animation_player.play("idleDown")
 
-func _unhandled_input(event: InputEvent) -> void:
+func _physics_process(delta: float) -> void:
+	holdManager()
 	inputDirection = Vector2.ZERO
-	if event.is_action_pressed("ui_left"): inputDirection = Vector2.LEFT
-	if event.is_action_pressed("ui_right"): inputDirection = Vector2.RIGHT
-	if event.is_action_pressed("ui_up"): inputDirection = Vector2.UP
-	if event.is_action_pressed("ui_down"): inputDirection = Vector2.DOWN
 
-	if inputDirection == Vector2.ZERO: return
+	# loop through all the directions ('ui_up', ui_left', etc),
+	# check to see if a key has been released, and if so, we
+	# want to remove it from the array holding all the pressed keys
+	for direction in MOVEMENTS.keys():
+		if Input.is_action_just_released(direction):
+			var index = direction_history.find(direction)
+			if index != -1:
+				direction_history.remove_at(index)
+		if Input.is_action_just_pressed(direction):
+			direction_history.append(direction)
 
-	if not currentFacingDirection.is_equal_approx(inputDirection):
+	if direction_history.size():
+		var direction = direction_history[direction_history.size() - 1]
+		inputDirection = MOVEMENTS[direction]
+
+	if inputDirection == Vector2.ZERO: return # don't do anything if there's no input
+	
+	if !currentFacingDirection.is_equal_approx(inputDirection):
 		updateFacingDirection()
 	else:
 		move()
 
+func holdManager():
+	if inputDirection != Vector2.ZERO:
+		holdCounter += 1
+	else: holdCounter = 0
+	
+	if holdCounter > holdTime: return true
+	else: return false
+
 func move():
 	if inputDirection:
-		if isMoving == false and !rayCast.is_colliding():
+		if isMoving == false and !rayCast.is_colliding() and holdManager():
 			isMoving = true
 			var tween = create_tween()
 			tween.tween_property(self, "position", position+inputDirection*Globals.TILE_SIZE, moveSpeed)
-			
-			# animate walk here
-			
 			tween.tween_callback(stopMoving)
 			
 func stopMoving():
 	isMoving = false
-	updateFacingDirection()
 
 func updateFacingDirection():
 	if inputDirection == Vector2.UP:
