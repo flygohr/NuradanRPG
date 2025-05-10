1eaa77eda57fd8cc0cf8826dd6aa5367c9fe9e24
Flygohr <info@flygohr.com>
2025-05-10 15:54:18 +0200

    Implemented input buffering from PR 1 and did some other tweaks sry if bad etiquette idk how to use git

--- Player/player.gd
+++ Player/player.gd
@@ -2,7 +2,7 @@ extends Sprite2D
 
 class_name Player
 
-@export var walk_speed = 6.0
+@export var walk_speed = 7.0
 
 @onready var animTree = $AnimationTree
 @onready var animState = animTree.get("parameters/playback")
@@ -20,15 +20,46 @@ var inputDirection = Vector2(0, 0)
 var isMoving = false
 var percentMovedToNextTile = 0.0
 
+# Keep track of the latest direction the player wants to move to
+var latestDirection = null
+var holdCounter := 0.0
+var idleCounter := 0.0
+var holdTarget := 0.2
+
+# Utility mapping from direction to vector
+const MOVEMENTS = {
+	facingDirections.UP: Vector2.UP,
+	facingDirections.LEFT: Vector2.LEFT,
+	facingDirections.RIGHT: Vector2.RIGHT,
+	facingDirections.DOWN: Vector2.DOWN,
+ }
+
 func _ready():
 	animTree.active = true
 	initialPosition = position
 
 func _physics_process(delta):
+	if idleCounter >= holdTarget: # resets holdCounter if idle for long
+		holdCounter = 0.0
+		idleCounter = 0.0
+	# First we register which direction the player intents to move to
+	if Input.is_action_just_pressed("ui_left"):
+		latestDirection = facingDirections.LEFT
+	elif Input.is_action_just_pressed("ui_right"):
+		latestDirection = facingDirections.RIGHT
+	elif Input.is_action_just_pressed("ui_down"):
+		latestDirection = facingDirections.DOWN
+	elif Input.is_action_just_pressed("ui_up"):
+		latestDirection = facingDirections.UP
+
+	# If they release a button, they cancel their intent
+	if Input.is_action_just_released("ui_up") or Input.is_action_just_released("ui_left") or Input.is_action_just_released("ui_right") or Input.is_action_just_released("ui_down"):
+		latestDirection = null
+		
 	if currentPlayerState == playerStates.TURNING:
 		return
 	elif isMoving == false:
-		process_player_input()
+		process_player_input(delta)
 	elif inputDirection != Vector2.ZERO:
 		animState.travel("Walk")
 		move(delta)
@@ -47,17 +78,23 @@ func change_front_check_position(dir):
 		Vector2.LEFT:	
 			frontCheckBox.position = Vector2(-16, -16)
 
-func process_player_input():
+func process_player_input(delta):
+
 	if inputDirection.y == 0:
 		inputDirection.x = int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left"))
 	if inputDirection.x == 0:
 		inputDirection.y = int(Input.is_action_pressed("ui_down")) - int(Input.is_action_pressed("ui_up"))
+
+	# If the player had intent to move, use that instead of the "outdated" inputDirection
+	if latestDirection != null:
+		inputDirection = MOVEMENTS[latestDirection]
+
 	if inputDirection != Vector2.ZERO:
 		animTree.set("parameters/Idle/blend_position", inputDirection)
 		animTree.set("parameters/Walk/blend_position", inputDirection)
 		animTree.set("parameters/Turn/blend_position", inputDirection)
 		change_front_check_position(inputDirection)
-		if need_to_turn():
+		if need_to_turn() and holdCounter <= holdTarget:
 			currentPlayerState = playerStates.TURNING
 			animState.travel("Turn")
 		else:
@@ -65,6 +102,7 @@ func process_player_input():
 			isMoving = true
 	else:
 		animState.travel("Idle")
+		idleCounter += delta
 
 func need_to_turn():
 	var newCurrentFacingDirection
@@ -87,6 +125,7 @@ func finished_turning():
 	currentPlayerState = playerStates.IDLE
 
 func move(delta):
+	holdCounter += delta
 	var desiredStep: Vector2 = inputDirection * Globals.TILE_SIZE / 2
 	rayCast.target_position = desiredStep
 	rayCast.force_raycast_update()
