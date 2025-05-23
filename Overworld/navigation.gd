9f3cbba4ca1578e52d10ccd8b1a2c55a2e7fa19d
Flygohr <info@flygohr.com>
2025-05-23 14:35:32 +0200

    still stuck on map loading

--- Overworld/navigation.gd
+++ Overworld/navigation.gd
@@ -2,7 +2,14 @@ extends Node
 
 var current_connections_list := []  # a list of all the areas that currently need to be displayed, their position already globalized by the maps themselves
 
-# currently, loading all the connections works fine BUT
-# I need to unload only the maps that currently don't connect
-# maybe this navigation manager should do it at runtime?
-# the unloading is the thing that doesn't work here
+func load_connection(resource_path): # loads unique resource paths to later load as maps?
+	if !current_connections_list.has(resource_path):
+		current_connections_list.append(resource_path)
+	else: return
+	
+func unload_connection(resource_path): # unloads a resource path only if it's not in the active list
+	current_connections_list.erase(resource_path)
+
+func _physics_process(delta: float) -> void:
+	# load threads here
+	pass
