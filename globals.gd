4fe362146f917a42de7b43bf3055766f061087df
flygohr <info@flygohr.com>
2026-04-10 22:11:00 +0200

    some adjustments before diving deeper

--- globals.gd
+++ globals.gd
@@ -6,6 +6,12 @@ var CAMERA_ZOOM := 1 # Default is 4
 var PLAYER_SPEED := 7 # Default is 7
 var ZONE_DETECTION_AREA_INCREASE := 0 # Extra tiles inbetween zones detection (for preloading and waiting to deload)
 
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("Fullscreen"):
+		var mode := DisplayServer.window_get_mode()
+		var is_window: bool = mode != DisplayServer.WINDOW_MODE_FULLSCREEN
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN if is_window else DisplayServer.WINDOW_MODE_WINDOWED)
+
 enum ZONE_NAMES { 
 	SUND, 
 	SUND_FIELDS, 
---
+++ readme.md
@@ -0,0 +1,17 @@
+
+
+### Devlog:
+- 2026-04-10
+	- Coming back to the project after a long while
+	- Project doesn't start, needs some refactoring, gonna take a while
+	- Targeting the web
+	
+### To-do's:
+- [ ] Implement features from my Godot template
+- [ ] Title screen
+- [ ] Settings menu
+- [ ] Mockup UI as in my latest sketches
+- [ ] Troubleshoot pre-existing code
+	- [ ] WebGL crashes
+	- [ ] Project feels heavy despite just being some pixels, need to inspect memory usage and other optimizations
+	- [ ] Signal zone_changed doesn't work anymore
